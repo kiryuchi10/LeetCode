a61b45b206538a8fd5454e84b4061f94da19768f
Dong Hyeun Lee <141597057+kiryuchi10@users.noreply.github.com>
2025-04-16 03:34:33 +0900

    Time: 39 ms (72.02%), Space: 53.2 MB (72.54%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,14 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let count = 0;
+    for (const _ of args) {
+        count++;
+    }
+    return count;
+};
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
