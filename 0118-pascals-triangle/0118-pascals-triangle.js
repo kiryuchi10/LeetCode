dea82bd08263754b7ae58026a200edab966242ba
Dong Hyeun Lee <141597057+kiryuchi10@users.noreply.github.com>
2025-07-08 23:36:52 +0900

    Time: 0 ms (100%), Space: 53.9 MB (77.32%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,16 +3,14 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    const result = [];
-    for (let i = 0; i < numRows; i++) {
-        const row = new Array(i + 1);
-        row[0] = 1;
-        row[i] = 1;
+    var result = [];
 
-        for (let j = 1; j < i; j++) {
+    for (let i = 0; i < numRows ; i++){
+        const row = new Array(i+1).fill(1);
+        for (let j = 1; j < i ; j++){
             row[j] = result[i - 1][j - 1] + result[i - 1][j];
         }
-
+        
         result.push(row);
     }
 
